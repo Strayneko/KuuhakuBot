3058df6083737559d2a79b05e034ebe0281389c8
Strayneko <bayurendi10@gmail.com>
2024-08-04 17:53:25 +0700

    Feat: create help handler

---
+++ handlers/help_handler.ts
@@ -0,0 +1,19 @@
+import config from "@/config/config";
+import getCommandList from "@/utils/get_command_list";
+import { APIEmbedField, EmbedBuilder, Message } from "discord.js";
+
+export default function helpHandler(msg: Message, cmdArg: string) {
+    const commandList = getCommandList().map((cmd, i) => {
+        return  `**${i + 1}.** \`${config.BOT_PREFIX}${cmd.name}\` or \`${config.BOT_PREFIX}${
+            cmd.shortName
+          }\` -- ${cmd.description}`;
+    }).join("\n");
+
+    const embed = new EmbedBuilder({
+        color: config.EMBED_COLOR.Primary,
+        title: 'Avaialle Commands',
+        description: commandList,
+    });
+
+    msg.channel.send({ embeds: [embed] });
+}
