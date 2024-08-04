c7bd19d69a8cc21274898984d6b7a14ec6d1e5d4
Strayneko <bayurendi10@gmail.com>
2024-08-04 16:18:27 +0700

    Feat: create utils to format duration

---
+++ utils/duration_formatter.ts
@@ -0,0 +1,15 @@
+export default function durationFormatter(duration: number): string {
+    const seconds: number = Math.floor((duration / 1000) % 60);
+    const minutes: number = Math.floor(duration / (1000 * 60));
+    const hours: number = Math.floor(duration / (1000 * 60 * 60));
+
+    const hoursFormat: string = addZero(hours);
+    const minutesFormat: string = addZero(minutes);
+    const secondsFormat: string = addZero(seconds);
+
+    return (hours > 0) ? `${hoursFormat}h:${minutesFormat}m:${secondsFormat}s` : `${minutesFormat}m:${secondsFormat}s`;
+}
+
+function addZero(time: number): string {
+    return (time < 10) ? "0" + time : `${time}`;
+}
