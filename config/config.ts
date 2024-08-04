0fd885b4b5ae12f7a2ba9680282749ad46298704
Strayneko <bayurendi10@gmail.com>
2024-08-04 16:18:48 +0700

    Feat: config for redis and lang

--- config/config.ts
+++ config/config.ts
@@ -6,6 +6,18 @@ const config = {
     BOT_PREFIX: process.env.DISCORD_PREFIX || '',
     DISCORD_TOKEN: process.env.DISCORD_TOKEN || '',
     BOT_ID: '',
+    EMBED_COLOR: {
+        Success: 0x00fa9a,
+        Error: 0xff2a16,
+        Warning: 0xffd700,
+        Info: 0x00bfaf,
+        Primary: 0x3498db,
+    },
+    REDIS: {
+        HOST: process.env.REDIS_HOST,
+        PORT: Number(process.env.REDIS_PORT),
+        PASSWORD: process.env.REDIS_PASSWORD || '',
+    },
 }
 
 export default config
---
+++ config/lang.ts
@@ -0,0 +1,14 @@
+const EN = {
+    YT_SEARCH: {
+        FAILED: ":sob: Shiro can't access youtube right now.",
+        NO_RESULT: ":sob: Shiro can't find your request on youtube."
+    },
+    YTDL: {
+        NO_AUDIO_FORMATS: ":sob: No audio format for this youtube video, Shiro can't sing at the moment."
+    },
+    VOICE: {
+        NOT_CONNECTED: ":sneeze: You have to join the voice channel if you want to hear Shiro sing."
+    }
+}
+
+export default { EN }
