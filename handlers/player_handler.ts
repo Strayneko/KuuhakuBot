375748538fc109484f6e77f5a274121c981b40e6
Strayneko <bayurendi10@gmail.com>
2024-08-04 16:19:06 +0700

    Feat: add redis query chache

---
+++ handlers/player_handler.ts
@@ -0,0 +1,63 @@
+import { Player } from "discord-player";
+import { EmbedBuilder, type Client } from "discord.js";
+import { YoutubeiExtractor } from "discord-player-youtubei"
+import { RedisQueryCache } from "@/class/QueryCache";
+import { Redis } from "ioredis";
+import config from "@/config/config";
+
+export default async function initPlayer(client: Client): Promise<Player> {
+    const redis = await initRedis()
+    const player = new Player(client, {
+        skipFFmpeg: false,
+        queryCache: new RedisQueryCache(redis),
+    })
+
+    player.extractors.register(YoutubeiExtractor, {})
+    getPlayerHandlers(player)
+    return player;
+}
+
+async function initRedis(): Promise<Redis> {
+    const redis = new Redis({
+        lazyConnect: true,
+        port: config.REDIS.PORT,
+        host: config.REDIS.HOST,
+        password: config.REDIS.PASSWORD,
+    });
+  
+  await redis.connect();
+  
+  console.log('Connected to Redis');
+
+  return redis;
+}
+
+function getPlayerHandlers(player: Player) {
+    player.on('debug', console.log);
+    
+    player.events.on('playerStart', (queue, track) => {
+        const embed = new EmbedBuilder({
+            color: config.EMBED_COLOR.Primary,
+            thumbnail: { url: track.thumbnail },
+            fields: [
+                {
+                    name:   "Playing track",
+                    value:  `[${track.title}](${track.url}) - **${track.duration}**`,
+                    inline: true,
+                },
+            ],
+        });
+
+        setTimeout(() => {
+            queue.metadata?.channel?.send({embeds: [embed]});
+        }, 500)
+    })
+    
+    player.events.on('playerError', (queue, error, track) => {
+        console.error(error)
+    })
+
+    player.events.on('error', (error) => {
+        console.error(error)
+    })
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-// import discord.js
-import {Client, Events, GatewayIntentBits, IntentsBitField} from 'discord.js';
-import config, { setBotId } from './config/config';
-import messageHandler from './handlers/message_handler';
+import 'module-alias/register';
+import {Client, EmbedBuilder, Events, IntentsBitField} from 'discord.js';
+import config, { setBotId } from '@/config/config';
+import messageHandler from '@/handlers/message_handler';
+import initPlayer from '@/handlers/player_handler';
 
 // create a new Client instance
 const client = new Client({
@@ -10,16 +11,20 @@ const client = new Client({
         IntentsBitField.Flags.GuildMessages, 
         IntentsBitField.Flags.Guilds, 
         IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildVoiceStates,
     ],
 });
 
-// listen for the client to be ready
+(async () => {
+    await initPlayer(client);
+    
+    // listen for the client to be ready
+    client.once(Events.ClientReady, (c) => {
+       setBotId(c.user.id)
+       console.log(`Bot is now running. Press CTRL-C to exit. Logged in as ${c.user.tag}`);
+    });
 
-client.once(Events.ClientReady, (c) => {
-   setBotId(c.user.id)
-   console.log(`Bot is now running. Press CTRL-C to exit. Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.MessageCreate, messageHandler)
-// login with the token from .env.local
-client.login(config.DISCORD_TOKEN);
+    client.on(Events.MessageCreate, messageHandler)
+    // login with the token from .env.local
+    client.login(config.DISCORD_TOKEN);
+})()
