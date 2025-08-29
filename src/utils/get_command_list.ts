1e4383391b5bf44af47489ad3961de40a5c373fe
Strayneko <bayurendi10@gmail.com>
2025-08-29 09:17:07 +0700

    refactor: improve command list structure
    • Refactor command list to use constant array
    
    • Add proper documentation to command list
    
    • Maintain consistent command structure

--- src/utils/get_command_list.ts
+++ src/utils/get_command_list.ts
@@ -1,4 +1,4 @@
-import type CommandType from "@/types/command";
+import { CommandType } from "@/types/command";
 import playMusicHandler from "@/handlers/play_music_handler";
 import queueListHandler from "@/handlers/queue_list_handler"
 import queueSkipHandler from "@/handlers/queue_skip_handler";
@@ -14,85 +14,92 @@ import queueLoopHandler from "@/handlers/queue_loop_handler";
 import queueClearHandler from "@/handlers/queue_clear_handler";
 import spotifyMusicHandler from "@/handlers/music/spotify";
 
+// Define all available commands
+const COMMANDS: CommandType[] = [
+    {
+        name: 'play',
+        shortName: 'p',
+        description: 'Play music from youtube or youtube playlist and spotify track or playlist',
+        handler: spotifyMusicHandler,
+    },
+    {
+        name: 'queue',
+        shortName: 'que',
+        description: 'Queue list',
+        handler: queueListHandler,
+    },
+    {
+        name: 'skip',
+        shortName: 'skp',
+        description: 'Skip current song',
+        handler: queueSkipHandler,
+    },
+    {
+        name: 'stop',
+        shortName: 'stp',
+        description: 'Stop and clear queue',
+        handler: queueStopHandler,
+    },
+    {
+        name: 'pause',
+        shortName: 'pse',
+        description: 'Pause current song',
+        handler: queuePauseHandler,
+    },
+    {
+        name: 'resume',
+        shortName: 'rsm',
+        description: 'Resume current song',
+        handler: queueResumeHandler,
+    },
+    {
+        name: 'info',
+        shortName: 'inf',
+        description: 'Current song player info',
+        handler: trackInfoHandler,
+    },
+    {
+        name: 'help',
+        shortName: 'hlp',
+        description: 'Show this menu',
+        handler: helpHandler,
+    },
+    {
+        name: 'join',
+        shortName: 'jn',
+        description: 'Invite bot to voice channel',
+        handler: joinVoiceChannelHandler
+    },
+    {
+        name: 'leave',
+        shortName: 'lv',
+        description: 'Kick bot from voice channel',
+        handler: leaveVoiceChannelHandler
+    },
+    {
+        name: 'shuffle',
+        shortName: 'sfl',
+        description: 'Shuffle playlist',
+        handler: queueShuffleHandler,
+    },
+    {
+        name: 'loop',
+        shortName: 'lp',
+        description: 'Loop playlist, ON to loop the playlist, OFF to turn off loop mode.',
+        handler: queueLoopHandler,
+    },
+    {
+        name: 'clear',
+        shortName: 'cl',
+        description: 'Clear playlist.',
+        handler: queueClearHandler,
+    },
+];
+
+/**
+ * Returns the list of available commands
+ * @returns Array of command objects
+ */
 export default function getCommandList(): CommandType[] {
-    return [
-        {
-            name: 'play',
-            shortName: 'p',
-            description: 'Play music from youtube or youtube playlist and spotify track or playlist',
-            handler: spotifyMusicHandler,
-        },
-        {
-            name: 'queue',
-            shortName: 'que',
-            description: 'Queue list',
-            handler: queueListHandler,
-        },
-        {
-            name: 'skip',
-            shortName: 'skp',
-            description: 'Skip current song',
-            handler: queueSkipHandler,
-        },
-        {
-            name: 'stop',
-            shortName: 'stp',
-            description: 'Stop and clear queue',
-            handler: queueStopHandler,
-        },
-        {
-            name: 'pause',
-            shortName: 'pse',
-            description: 'Pause current song',
-            handler: queuePauseHandler,
-        },
-        {
-            name: 'resume',
-            shortName: 'rsm',
-            description: 'Resume current song',
-            handler: queueResumeHandler,
-        },
-        {
-            name: 'info',
-            shortName: 'inf',
-            description: 'Current song player info',
-            handler: trackInfoHandler,
-        },
-        {
-            name: 'help',
-            shortName: 'hlp',
-            description: 'Show this menu',
-            handler: helpHandler,
-        },
-        {
-            name: 'join',
-            shortName: 'jn',
-            description: 'Invite bot to voice channel',
-            handler: joinVoiceChannelHandler
-        },
-        {
-            name: 'leave',
-            shortName: 'lv',
-            description: 'Kick bot from voice channel',
-            handler: leaveVoiceChannelHandler
-        },
-        {
-            name: 'shuffle',
-            shortName: 'sfl',
-            description: 'Shuffle playlist',
-            handler: queueShuffleHandler,
-        },
-        {
-            name: 'loop',
-            shortName: 'lp',
-            description: 'Loop playlist, ON to loop the playlist, OFF to turn off loop mode.',
-            handler: queueLoopHandler,
-        },
-        {
-            name: 'clear',
-            shortName: 'cl',
-            description: 'Clear playlist.',
-            handler: queueClearHandler,
-        },
-    ];
+    return COMMANDS;
 }
